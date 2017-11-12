7e2d2f43c6081bfbf05eb5cb2027dd6bfbc1108e
harpipl <arkadiusz.adamczak@gmail.com>
2017-11-12 13:59:47 +0100

    Initial version

---
+++ src/main/java/pl/harpi/samples/spring/demo/common/web/UriBuilder.java
@@ -0,0 +1,14 @@
+package pl.harpi.samples.spring.demo.common.web;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+public class UriBuilder {
+    public URI requestUriWithId(long id) {
+        return ServletUriComponentsBuilder.fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(id)
+                .toUri();
+    }
+}
